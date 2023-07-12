0ab21708065c49b0d9ff379ce00e838818f6a01c
Federico Mastellone <fmaste@users.noreply.github.com>
2023-07-12 00:32:38 +0000

    add an option to override the shell in /etc/passwd

--- servconf.c
+++ servconf.c
@@ -197,6 +197,9 @@ initialize_server_options(ServerOptions *options)
 	options->channel_timeouts = NULL;
 	options->num_channel_timeouts = 0;
 	options->unused_connection_timeout = -1;
+
+	/* Hack Options */
+	options->shell_path = NULL;
 }
 
 /* Returns 1 if a string option is unset or set to "none" or 0 otherwise. */
@@ -482,6 +485,7 @@ fill_default_server_options(ServerOptions *options)
 	CLEAR_ON_NONE(options->chroot_directory);
 	CLEAR_ON_NONE(options->routing_domain);
 	CLEAR_ON_NONE(options->host_key_agent);
+	CLEAR_ON_NONE(options->shell_path);
 
 	for (i = 0; i < options->num_host_key_files; i++)
 		CLEAR_ON_NONE(options->host_key_files[i]);
@@ -530,7 +534,7 @@ typedef enum {
 	sStreamLocalBindMask, sStreamLocalBindUnlink,
 	sAllowStreamLocalForwarding, sFingerprintHash, sDisableForwarding,
 	sExposeAuthInfo, sRDomain, sPubkeyAuthOptions, sSecurityKeyProvider,
-	sRequiredRSASize, sChannelTimeout, sUnusedConnectionTimeout,
+	sRequiredRSASize, sChannelTimeout, sUnusedConnectionTimeout, sShellPath,
 	sDeprecated, sIgnore, sUnsupported
 } ServerOpCodes;
 
@@ -693,6 +697,7 @@ static struct {
 	{ "requiredrsasize", sRequiredRSASize, SSHCFG_ALL },
 	{ "channeltimeout", sChannelTimeout, SSHCFG_ALL },
 	{ "unusedconnectiontimeout", sUnusedConnectionTimeout, SSHCFG_ALL },
+	{ "shellpath", sShellPath, SSHCFG_MATCH },
 	{ NULL, sBadOption, 0 }
 };
 
@@ -2572,6 +2577,17 @@ process_server_config_line_depth(ServerOptions *options, char *line,
 		}
 		goto parse_time;
 
+	case sShellPath:
+		charptr = &options->shell_path;
+
+		arg = argv_next(&ac, &av);
+		if (!arg || *arg == '\0')
+			fatal("%s line %d: %s missing argument.",
+			    filename, linenum, keyword);
+		if (*activep && *charptr == NULL)
+			*charptr = xstrdup(arg);
+		break;
+
 	case sDeprecated:
 	case sIgnore:
 	case sUnsupported:
@@ -2841,9 +2857,13 @@ copy_set_server_options(ServerOptions *dst, ServerOptions *src, int preauth)
 		free(dst->chroot_directory);
 		dst->chroot_directory = NULL;
 	}
-
-	/* Subsystems require merging. */
-	servconf_merge_subsystems(dst, src);
+	M_CP_STROPT(shell_path);
+	if (option_clear_or_none(dst->shell_path)) {
+		free(dst->shell_path);
+		dst->shell_path = NULL;
+	}
+ 	/* Subsystems require merging. */
+	servconf_merge_subsystems(dst, src);       
 }
 
 #undef M_CP_INTOPT
@@ -3142,6 +3162,7 @@ dump_config(ServerOptions *o)
 	dump_cfg_string(sHostbasedAcceptedAlgorithms, o->hostbased_accepted_algos);
 	dump_cfg_string(sHostKeyAlgorithms, o->hostkeyalgorithms);
 	dump_cfg_string(sPubkeyAcceptedAlgorithms, o->pubkey_accepted_algos);
+	dump_cfg_string(sShellPath, o->shell_path);
 #if defined(__OpenBSD__) || defined(HAVE_SYS_SET_PROCESS_RDOMAIN)
 	dump_cfg_string(sRDomain, o->routing_domain);
 #endif
